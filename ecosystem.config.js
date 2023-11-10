dfdf5bb1b1b39ea9df8f6a805d80afcc602a9d2e
Sammy Guttman <samisosa20@hotmail.com>
2023-11-10 08:37:31 -0500

    feat: changes deploy settings

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
         exec_mode: 'cluster',
         instances: '1', // Or a number of instances
         script: 'node_modules/next/dist/bin/next',
-        args: 'start -p 3030',
+        args: 'start -p ' + process.env.NEXT_PUBLIC_PORT,
         env_local: {
           APP_ENV: 'local' // APP_ENV=local
         },
