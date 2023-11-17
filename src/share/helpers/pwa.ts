49657698059381b893d684b35643a4bedc2c4ef8
Sammy Guttman <samisosa20@hotmail.com>
2023-11-17 17:43:47 -0500

    changes option to download

--- src/share/helpers/pwa.ts
+++ src/share/helpers/pwa.ts
@@ -1,4 +1,4 @@
-let deferredPrompt: any;
+var deferredPrompt: any;
 export function installPWA() {
   // Verificar si el navegador admite service workers
   if ("serviceWorker" in navigator) {
@@ -23,25 +23,18 @@ export function handleAddToHomeScreen() {
     console.log("La aplicación ya está instalada en la pantalla de inicio.");
   } else {
     if (deferredPrompt) {
-      // Mostrar un mensaje o un botón para instalar la PWA
-      var installButton = document.createElement("button");
-      installButton.textContent = "Instalar la aplicación";
-      installButton.addEventListener("click", function () {
-        // Solicitar la instalación de la PWA
-        deferredPrompt.prompt();
-        // Esperar a que el usuario responda a la solicitud
-        deferredPrompt.userChoice.then(function (choiceResult: any) {
-          if (choiceResult.outcome === "accepted") {
-            console.log("El usuario ha aceptado instalar la aplicación");
-          } else {
-            console.log("El usuario ha rechazado instalar la aplicación");
-          }
-          // Limpiar la referencia al evento de instalación diferida
-          deferredPrompt = null;
-        });
+      // Solicitar la instalación de la PWA
+      deferredPrompt.prompt();
+      // Esperar a que el usuario responda a la solicitud
+      deferredPrompt.userChoice.then(function (choiceResult: any) {
+        if (choiceResult.outcome === "accepted") {
+          console.log("El usuario ha aceptado instalar la aplicación");
+        } else {
+          console.log("El usuario ha rechazado instalar la aplicación");
+        }
+        // Limpiar la referencia al evento de instalación diferida
+        deferredPrompt = null;
       });
-      // Mostrar el botón de instalación en la interfaz de usuario
-      document.body.appendChild(installButton);
     }
   }
 }
