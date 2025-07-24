226f1ae094cedd747fa2297bfedd53cc58e66291
Sammy Guttman <samisosa20@hotmail.com>
2025-07-24 17:43:34 +0200

    feat: disable refresh

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,44 +1,46 @@
-'use client';
-import { useEffect } from 'react';
-import { useRouter, usePathname } from 'next/navigation';
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
+"use client";
+import { useEffect } from "react";
+import { useRouter, usePathname } from "next/navigation";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { isLogin } from '@/share/helpers';
-
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+    },
+  },
+});
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   const pathname = usePathname();
   useEffect(() => {
     //if (!isLogin() && pathname !== '/' && pathname !== '/login' && pathname !== '/register' && pathname !== '/forgot' && !pathname.includes('/blogs')) router.push('/login');
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker
+          .register("/sw.js")
           .then((registration) => {
             //console.log('Service Worker registrado con éxito:', registration);
           })
           .catch((error) => {
-            console.error('Error al registrar el Service Worker:', error);
+            console.error("Error al registrar el Service Worker:", error);
           });
       });
     }
-    if ('Notification' in window) {
+    if ("Notification" in window) {
       Notification.requestPermission().then((permission) => {
-        if (permission === 'granted') {
+        if (permission === "granted") {
           // El usuario otorgó permiso para mostrar notificaciones
-        } else if (permission === 'denied') {
+        } else if (permission === "denied") {
           // El usuario denegó el permiso
         }
       });
     }
-  }, [pathname])
+  }, [pathname]);
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
